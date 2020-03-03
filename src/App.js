15a398eff08616d10c8f2ada9d053a819e653f5a
Jason Rabbitt <jrabbitt@deseretdigital.com>
2020-03-03 13:41:24 -0700

    Got amcharts to work with Ragnar data.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,161 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import wasatchback from './assets/wasatchback'
+import { calculateMapPoints } from './helpers/legElevations'
+
+am4core.useTheme(am4themes_animated);
+
+class App extends Component {
+  componentDidMount() {
+    // let chart = am4core.create("chartdiv", am4charts.XYChart);
+
+    // chart.paddingRight = 20;
+
+    // let data = [];
+
+    // let visits = 10;
+    // for (let i = 1; i < 366; i++) {
+    //   visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+    //   data.push({ date: new Date(2018, 0, i), name: "name" + i, value: visits });
+    // }
+
+    // chart.data = data;
+
+    // let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+    // dateAxis.renderer.grid.template.location = 0;
+
+    // let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    // valueAxis.tooltip.disabled = true;
+    // valueAxis.renderer.minWidth = 35;
+
+    // let series = chart.series.push(new am4charts.LineSeries());
+    // series.dataFields.dateX = "date";
+    // series.dataFields.valueY = "value";
+
+    // series.tooltipText = "{valueY.value}";
+    // chart.cursor = new am4charts.XYCursor();
+
+    // let scrollbarX = new am4charts.XYChartScrollbar();
+    // scrollbarX.series.push(series);
+    // chart.scrollbarX = scrollbarX;
+
+    // this.chart = chart;
+
+
+    // Legs Chart
+    let legChart = am4core.create("legdiv", am4charts.XYChart);
+    let legData = []
+    let raceData = []
+    let raceCalc = []
+
+    let legs = wasatchback.data.legs;
+    legs.forEach(leg => {
+      let points = leg.points;
+
+      leg.points.forEach(point => {
+        raceData.push(point);
+      })
+
+      let leg_number = leg.leg_number;
+      legData.push({leg_number, distanceToElevation: calculateMapPoints(points)})
+    });
+
+
+    //legChart.data = legData
+    let distanceAxis = legChart.xAxes.push(new am4charts.ValueAxis());
+    let elevationAxis = legChart.yAxes.push(new am4charts.ValueAxis());
+
+    let leg1 = legChart.series.push(new am4charts.LineSeries());
+    leg1.dataFields.valueX = "distance"
+    leg1.dataFields.valueY = "elevation"
+    leg1.strokeWidth = 1
+    leg1.tooltipText = "{valueX.value}"
+    leg1.stroke = am4core.color("#ff0000")
+    leg1.data = legData[0]['distanceToElevation']
+
+    let leg2 = legChart.series.push(new am4charts.LineSeries());
+    leg2.dataFields.valueX = "distance"
+    leg2.dataFields.valueY = "elevation"
+    leg2.strokeWidth = 1
+    leg2.stroke = am4core.color("#0000ff")
+    leg2.data = legData[1]['distanceToElevation']
+
+    let leg3 = legChart.series.push(new am4charts.LineSeries());
+    leg3.dataFields.valueX = "distance"
+    leg3.dataFields.valueY = "elevation"
+    leg3.strokeWidth = 1
+    leg3.stroke = am4core.color("#00ff00")
+    leg3.data = legData[2]['distanceToElevation']
+
+    let leg4 = legChart.series.push(new am4charts.LineSeries());
+    leg4.dataFields.valueX = "distance"
+    leg4.dataFields.valueY = "elevation"
+    leg4.strokeWidth = 1
+    leg4.stroke = am4core.color("#00ffff")
+    leg4.data = legData[3]['distanceToElevation']
+
+    let leg5 = legChart.series.push(new am4charts.LineSeries());
+    leg5.dataFields.valueX = "distance"
+    leg5.dataFields.valueY = "elevation"
+    leg5.strokeWidth = 1
+    leg5.stroke = am4core.color("#ffaa00")
+    leg5.data = legData[4]['distanceToElevation']
+
+    let leg6 = legChart.series.push(new am4charts.LineSeries());
+    leg6.dataFields.valueX = "distance"
+    leg6.dataFields.valueY = "elevation"
+    leg6.strokeWidth = 1
+    leg6.stroke = am4core.color("#ff00aa")
+    leg6.data = legData[5]['distanceToElevation']
+
+
+    this.legChart = legChart;
+
+
+    // Ragnar Race Chart
+    let ragnarChart = am4core.create("ragnardiv", am4charts.XYChart);
+
+    raceCalc.push({distanceToElevation: calculateMapPoints(raceData)});
+
+    console.log(raceCalc);
+
+
+    //ragnarChart.data = legData
+    let raceDistanceAxis = ragnarChart.xAxes.push(new am4charts.ValueAxis());
+    let raceElevationAxis = ragnarChart.yAxes.push(new am4charts.ValueAxis());
+
+    let ragnarData = ragnarChart.series.push(new am4charts.LineSeries());
+    ragnarData.dataFields.valueX = "distance"
+    ragnarData.dataFields.valueY = "elevation"
+    ragnarData.strokeWidth = 1
+    ragnarData.tooltipText = "{valueX.value}"
+    ragnarData.stroke = am4core.color("#ddaa00")
+    ragnarData.data = raceCalc[0]['distanceToElevation'];
+
+    this.ragnarChart = ragnarChart;
+
+  }
+
+  componentWillUnmount() {
+    if (this.chart) {
+      this.chart.dispose();
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {/* <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div> */}
+        <div id="legdiv" style={{ width: "100%", height: "500px" }}></div>
+        <div id="ragnardiv" style={{ width: "100%", height: "500px" }}></div>
+      </div>
+      
+    );
+  }
 }
 
 export default App;
---
+++ src/helpers/legElevations.js
@@ -0,0 +1,41 @@
+const calDistance = (lat1, lon1, lat2, lon2, unit) => {
+	if ((lat1 === lat2) && (lon1 === lon2)) {
+		return 0;
+	}
+	else {
+		var radlat1 = Math.PI * lat1/180;
+		var radlat2 = Math.PI * lat2/180;
+		var theta = lon1-lon2;
+		var radtheta = Math.PI * theta/180;
+		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+		if (dist > 1) {
+			dist = 1;
+		}
+		dist = Math.acos(dist);
+		dist = dist * 180/Math.PI;
+		dist = dist * 60 * 1.1515;
+		if (unit==="K") { dist = dist * 1.609344 }
+		if (unit==="N") { dist = dist * 0.8684 }
+		return dist;
+	}
+}
+
+export const calculateMapPoints = points => {
+    let distance = 0
+    let i = 0
+    let prevLat, prevLon
+    let calculatedMapPoints = points.map(data => {
+        if (i === 0) {distance = 0} else {
+            distance += calDistance(prevLat, prevLon, data.lat, data.lon, "M")
+        }
+        i = i + 1
+        prevLat = data.lat
+        prevLon = data.lon
+        var elevation = data.ele
+        var toFeet = parseFloat(elevation) * 39.37 /12
+        return ({distance, elevation: toFeet})
+        
+    })
+    
+    return calculatedMapPoints
+}
