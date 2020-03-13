9a40ef6a945deb62621646b76acbc7ec7d99b1eb
Jason Rabbitt <jrabbitt@deseretdigital.com>
2020-03-13 16:12:52 -0600

    Could not solve Heroku memory issue. Reverted to original app.

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ class App extends Component {
 
     let strokeWidth = 3;
 
-    // let fullRace =[]
-    // let race = []
+    let fullRace =[]
+    let race = []
     let legData = []
     let latLon = []
 
@@ -30,9 +30,9 @@ class App extends Component {
       runner_number === 0 ? runner_number = 12 : runner_number = runner_number + 0
 
       // For Full Race Chart
-      // leg.points.forEach(point => {
-      //   fullRace.push({leg_number: leg_number, runner_number, lat: point.lat, lon: point.lon, ele: point.ele});
-      // })
+      leg.points.forEach(point => {
+        fullRace.push({leg_number: leg_number, runner_number, lat: point.lat, lon: point.lon, ele: point.ele});
+      })
 
       // For Leg Charts
       legData.push({leg_number, distanceToElevation: calculateMapPoints(points)})
@@ -101,165 +101,165 @@ class App extends Component {
 
 
     // ----------- RAGNAR RACE CHART -------------
-    // let ragnarChart = am4core.create("ragnardiv", am4charts.XYChart);
+    let ragnarChart = am4core.create("ragnardiv", am4charts.XYChart);
 
-    // race.push(calculateMapPoints(fullRace))
+    race.push(calculateMapPoints(fullRace))
 
-    // let ragnarChartTitle = ragnarChart.titles.create();
-    // ragnarChartTitle.text = "Wasatch Back Full Race";
-    // ragnarChartTitle.fontSize = 25;
-    // ragnarChartTitle.marginTop = 20
-    // ragnarChartTitle.marginBottom = 10;
-    // ragnarChartTitle.align = "left";
+    let ragnarChartTitle = ragnarChart.titles.create();
+    ragnarChartTitle.text = "Wasatch Back Full Race";
+    ragnarChartTitle.fontSize = 25;
+    ragnarChartTitle.marginTop = 20
+    ragnarChartTitle.marginBottom = 10;
+    ragnarChartTitle.align = "left";
 
-    // let raceDistanceAxis = ragnarChart.xAxes.push(new am4charts.ValueAxis());
-    // raceDistanceAxis.title.text = "Distance (Miles)";
-    // let raceElevationAxis = ragnarChart.yAxes.push(new am4charts.ValueAxis());
-    // raceElevationAxis.title.text = "Elevation (Feet)";
+    let raceDistanceAxis = ragnarChart.xAxes.push(new am4charts.ValueAxis());
+    raceDistanceAxis.title.text = "Distance (Miles)";
+    let raceElevationAxis = ragnarChart.yAxes.push(new am4charts.ValueAxis());
+    raceElevationAxis.title.text = "Elevation (Feet)";
 
-    // let ragnarData = ragnarChart.series.push(new am4charts.LineSeries());
-    // ragnarData.dataFields.valueX = "distance"
-    // ragnarData.dataFields.valueY = "elevation"
-    // ragnarData.strokeWidth = strokeWidth
-    // ragnarData.name = "Full Race"
-    // ragnarData.stroke = am4core.color("#dfdfdf")
-    // ragnarData.data = race[0];
-    // ragnarData.hiddenInLegend = true
+    let ragnarData = ragnarChart.series.push(new am4charts.LineSeries());
+    ragnarData.dataFields.valueX = "distance"
+    ragnarData.dataFields.valueY = "elevation"
+    ragnarData.strokeWidth = strokeWidth
+    ragnarData.name = "Full Race"
+    ragnarData.stroke = am4core.color("#dfdfdf")
+    ragnarData.data = race[0];
+    ragnarData.hiddenInLegend = true
     
-    // ragnarChart.data = race[0];
+    ragnarChart.data = race[0];
 
-    // let scrollbarX = new am4charts.XYChartScrollbar();
-    // scrollbarX.series.push(ragnarData);
-    // ragnarChart.scrollbarX = scrollbarX;
+    let scrollbarX = new am4charts.XYChartScrollbar();
+    scrollbarX.series.push(ragnarData);
+    ragnarChart.scrollbarX = scrollbarX;
 
-    // ragnarChart.cursor = new am4charts.XYCursor();
-    // ragnarChart.cursor.lineY.disabled = true
-    // ragnarChart.cursor.lineX.strokeDasharray= "";
+    ragnarChart.cursor = new am4charts.XYCursor();
+    ragnarChart.cursor.lineY.disabled = true
+    ragnarChart.cursor.lineX.strokeDasharray= "";
 
-    // // Create Custom Tool Tip
-    // let customToolTip = ragnarChart.createChild(am4core.Tooltip);
-    // customToolTip.fontSize = 14;
-    // customToolTip.autoTextColor = false;
-    // customToolTip.label.fill = am4core.color("#000");
-    // customToolTip.background.fill = am4core.color("#ddd");
-    // customToolTip.background.opacity = ".8"
-    // customToolTip.pointerOrientation = "horizontal";
+    // Create Custom Tool Tip
+    let customToolTip = ragnarChart.createChild(am4core.Tooltip);
+    customToolTip.fontSize = 14;
+    customToolTip.autoTextColor = false;
+    customToolTip.label.fill = am4core.color("#000");
+    customToolTip.background.fill = am4core.color("#ddd");
+    customToolTip.background.opacity = ".8"
+    customToolTip.pointerOrientation = "horizontal";
 
-    // ragnarChart.plotContainer.events.on("out", function(ev) {
-    //     customToolTip.hide();
-    // });
+    ragnarChart.plotContainer.events.on("out", function(ev) {
+        customToolTip.hide();
+    });
 
-    // ragnarChart.plotContainer.events.on("over", function(ev) {
-    //     //customToolTip.appear();
-    //     customToolTip.show();
-    // });
+    ragnarChart.plotContainer.events.on("over", function(ev) {
+        //customToolTip.appear();
+        customToolTip.show();
+    });
 
-    // var last_idx = -1;
+    var last_idx = -1;
 
-    // ragnarChart.cursor.events.on("cursorpositionchanged", function(ev) {
-    //   // get cursor x coordinate
-    //   let xAxis = ev.target.chart.xAxes.getIndex(0);
-    //   let yAxis = ev.target.chart.yAxes.getIndex(0);
+    ragnarChart.cursor.events.on("cursorpositionchanged", function(ev) {
+      // get cursor x coordinate
+      let xAxis = ev.target.chart.xAxes.getIndex(0);
+      let yAxis = ev.target.chart.yAxes.getIndex(0);
       
-    //   let x = xAxis.positionToValue(xAxis.toAxisPosition(ev.target.xPosition));
+      let x = xAxis.positionToValue(xAxis.toAxisPosition(ev.target.xPosition));
       
-    //   // search closest data point
-    //   let idx = searchval(x);
+      // search closest data point
+      let idx = searchval(x);
       
-    //   // tooltip update only after data point change
-    //   if(idx !== last_idx) {
-    //     // data point coordinates
-    //     let xpos = xAxis.valueToPoint(ragnarChart.data[idx].distance);
-    //     let ypos = yAxis.valueToPoint(ragnarChart.data[idx].elevation);
-    //     // plot container offset
-    //     let xOffset = ragnarChart.plotContainer.pixelX;
-    //     let yOffset = ragnarChart.plotContainer.pixelY + 140;
+      // tooltip update only after data point change
+      if(idx !== last_idx) {
+        // data point coordinates
+        let xpos = xAxis.valueToPoint(ragnarChart.data[idx].distance);
+        let ypos = yAxis.valueToPoint(ragnarChart.data[idx].elevation);
+        // plot container offset
+        let xOffset = ragnarChart.plotContainer.pixelX;
+        let yOffset = ragnarChart.plotContainer.pixelY + 140;
         
-    //     let txt = round(ragnarChart.data[idx].elevation, 2) + " Feet\n" + round(ragnarChart.data[idx].distance, 2) + " Miles";
-    //     //txt = txt  + "\nxpos: "+ xpos.x + "\nypos: "+ypos.y + "\nxOff: " + xOffset;
-    //     //console.log(txt);
+        let txt = round(ragnarChart.data[idx].elevation, 2) + " Feet\n" + round(ragnarChart.data[idx].distance, 2) + " Miles";
+        //txt = txt  + "\nxpos: "+ xpos.x + "\nypos: "+ypos.y + "\nxOff: " + xOffset;
+        //console.log(txt);
 
-    //     // set content and move to data point
-    //     customToolTip.text = txt;
-    //     customToolTip.pointTo({"x": xpos.x + xOffset, "y": ypos.y + yOffset});
-    //     customToolTip.pointerOrientation = "left"
+        // set content and move to data point
+        customToolTip.text = txt;
+        customToolTip.pointTo({"x": xpos.x + xOffset, "y": ypos.y + yOffset});
+        customToolTip.pointerOrientation = "left"
         
-    //     last_idx = idx;
-    //   }
-    // });
+        last_idx = idx;
+      }
+    });
 
-    // function searchval(x) {
-    //     let center;
-    //     let left = 0;
-    //     let maxidx = ragnarChart.data.length - 1;
-    //     let right = maxidx;
+    function searchval(x) {
+        let center;
+        let left = 0;
+        let maxidx = ragnarChart.data.length - 1;
+        let right = maxidx;
         
-    //     // bisect data array 
-    //     // worst-case performance: O(log2(n))
-    //     while (left < right) {  
-    //         center = Math.floor((left + right) / 2);
+        // bisect data array 
+        // worst-case performance: O(log2(n))
+        while (left < right) {  
+            center = Math.floor((left + right) / 2);
 
-    //         if(ragnarChart.data[center].distance < x) {
-    //             left = center + 1;
-    //         }
-    //         else {
-    //             right = center;
-    //         }   
-    //     }
+            if(ragnarChart.data[center].distance < x) {
+                left = center + 1;
+            }
+            else {
+                right = center;
+            }   
+        }
         
-    //     // check which data point is closer to the cursor
-    //     if(x > ragnarChart.data[center].distance) {
-    //         if((center < maxidx) && (x - ragnarChart.data[center].distance) > (ragnarChart.data[center + 1].distance - x)) {
-    //             center = center + 1;
-    //         }
-    //     }   
+        // check which data point is closer to the cursor
+        if(x > ragnarChart.data[center].distance) {
+            if((center < maxidx) && (x - ragnarChart.data[center].distance) > (ragnarChart.data[center + 1].distance - x)) {
+                center = center + 1;
+            }
+        }   
 
-    //     if(x < ragnarChart.data[center].distance) {
-    //         if((center > 0) && (ragnarChart.data[center].distance - x) > (x - ragnarChart.data[center - 1].distance)) {
-    //             center = center - 1;
-    //         }
-    //     }
+        if(x < ragnarChart.data[center].distance) {
+            if((center > 0) && (ragnarChart.data[center].distance - x) > (x - ragnarChart.data[center - 1].distance)) {
+                center = center - 1;
+            }
+        }
         
-    //     return center;
-    // }
+        return center;
+    }
 
-    // function round(value, precision) {
-    //     var multiplier = Math.pow(10, precision || 0);
-    //     return Math.round(value * multiplier) / multiplier;
-    // }
+    function round(value, precision) {
+        var multiplier = Math.pow(10, precision || 0);
+        return Math.round(value * multiplier) / multiplier;
+    }
 
-    // let createRagnarSeries = (seriesData, name, color) => {
-    //   let ragnarSeries = ragnarChart.series.push(new am4charts.LineSeries());
-    //   ragnarSeries.dataFields.valueX = "distance"
-    //   ragnarSeries.dataFields.valueY = "elevation"
-    //   ragnarSeries.strokeWidth = strokeWidth
-    //   ragnarSeries.name = name
-    //   ragnarSeries.tooltipText = "{valueX.value}"
-    //   ragnarSeries.stroke = am4core.color(color)
-    //   ragnarSeries.connect = false
-    //   ragnarSeries.data = seriesData
+    let createRagnarSeries = (seriesData, name, color) => {
+      let ragnarSeries = ragnarChart.series.push(new am4charts.LineSeries());
+      ragnarSeries.dataFields.valueX = "distance"
+      ragnarSeries.dataFields.valueY = "elevation"
+      ragnarSeries.strokeWidth = strokeWidth
+      ragnarSeries.name = name
+      ragnarSeries.tooltipText = "{valueX.value}"
+      ragnarSeries.stroke = am4core.color(color)
+      ragnarSeries.connect = false
+      ragnarSeries.data = seriesData
 
-    //   return ragnarSeries
-    // }
+      return ragnarSeries
+    }
 
-    // for (let i=0; i < 12; i++) {
-    //   let runnerData = []
-    //   race[0].map(data => {
-    //     if (data.runner_number === i + 1) {
-    //       runnerData.push({distance: data.distance, elevation: data.elevation})
-    //     } else {
-    //       runnerData.push({})
-    //     }
-    //     return runnerData
-    //   })
-    //   let nameString = "Runner " + (i+1)
-    //   let colorString = runnerColors[i]
-    //   createRagnarSeries(runnerData, nameString, colorString)
-    // }
+    for (let i=0; i < 12; i++) {
+      let runnerData = []
+      race[0].map(data => {
+        if (data.runner_number === i + 1) {
+          runnerData.push({distance: data.distance, elevation: data.elevation})
+        } else {
+          runnerData.push({})
+        }
+        return runnerData
+      })
+      let nameString = "Runner " + (i+1)
+      let colorString = runnerColors[i]
+      createRagnarSeries(runnerData, nameString, colorString)
+    }
 
-    // ragnarChart.legend = new am4charts.Legend();
+    ragnarChart.legend = new am4charts.Legend();
 
-    // this.ragnarChart = ragnarChart;
+    this.ragnarChart = ragnarChart;
 
   }
 
@@ -272,7 +272,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        {/* <div id="ragnardiv" style={{ width: "100%", height: "500px" }}></div> */}
+        <div id="ragnardiv" style={{ width: "100%", height: "500px" }}></div>
         <hr></hr>
         <div id="legdiv" style={{ width: "100%", height: "500px" }}></div>
         <hr></hr>
